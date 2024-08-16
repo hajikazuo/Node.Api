abfad7e3b0682ee2e58f4efa5692f5ee5dba63da
hajikazuo <hajikazuo@outlook.com>
2024-08-16 17:55:24 -0300

    feat model e post

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
+mongoose.connect('mongodb+srv://hajikazuo:ndstr@cluster0.jn6y0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+const Product = require('./models/product');
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
 
