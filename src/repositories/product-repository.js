476dcfb11bf1390631485ab723ad183274aa6f7e
hajikazuo <hajikazuo@outlook.com>
2024-08-20 14:02:34 -0300

    fix async

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -3,51 +3,55 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
-exports.get = () => {
-    return Product
-        .find({
-            active: true
-        }, 'title price slug');
+exports.get = async () => {
+    const res = await Product.find({
+        active: true
+    }, 'title price slug');
+    return res;
 }
 
-exports.getBySlug = (slug) => {
-    return Product
+exports.getBySlug = async (slug) => {
+    const res = await Product
         .findOne({
             slug: slug,
             active: true
         }, 'title description price slug tags');
+    return res;
 }
 
-exports.getById = (id) => {
-    return Product
+exports.getById = async (id) => {
+    const res = await Product
         .findById(id);
+    return res;
 }
 
-exports.getByTag = (tag) => {
-    return Product
+exports.getByTag = async (tag) => {
+    const res = await Product
         .find({
             tags: tag,
             active: true
         }, 'title description price slug tags');
+    return res;
 }
 
-exports.create = (data) => {
+exports.create = async(data) => {
     var product = new Product(data);
-    return product.save();
+    await product.save();
 }
 
-exports.update = (id, data) => {
-    Product.findByIdAndUpdate(id,{
-        $set: {
-            title: data.title,
-            description: data.description,
-            price: data.price,
-            slug: data.slug
-        }
-    })
+exports.update = async(id, data) => {
+    await Product
+        .findByIdAndUpdate(id, {
+            $set: {
+                title: data.title,
+                description: data.description,
+                price: data.price,
+                slug: data.slug
+            }
+        })
 }
 
-exports.delete = (id) => {
-    return Product
-    .findOneAndDelete(id);
+exports.delete = async(id) => {
+    await Product
+        .findByIdAndDelete(id);
 }
